f7a46c427123ce99fef9c21d9844f9659f5a4d4f
Jan Schulz <jan.schulz@devugees.org>
2018-08-02 10:50:57 +0200

    added ajax example

---
+++ jquery/7-ajax/main.js
@@ -0,0 +1,32 @@
+$(document).ready( function() {
+    console.log('Document is loaded.');
+
+
+    // 1. use the $.ajax method, to load 
+    //    the data.json and load its contents
+    //    into the table #tbl1 (not the getJSON here)
+
+
+    // 2. create an ajax requests that 
+    //    loads a file that DOES NOT exist
+    //    and print out the status code in the "error" function
+    //    of the ajax object.
+
+
+    // 3. create an $.get request that loads
+    //    the "data.html" file into the #htmldata
+    //    div
+
+
+    // 4. load the script "data.js" using the
+    //    $.getScript function
+
+    // Same as in 1., load the data.txt again using the $.getJSON function 
+    // and load the contents into the table #tbl2
+
+    // 5. Load all users from http://35.156.88.18:3050/users
+
+    // 6. Create a new user on http://35.156.88.18:3050/users
+    
+});
+
