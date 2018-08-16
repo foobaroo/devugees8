e25b7163c25905610b01b3ae95b1e0798d0cba37
Jan Schulz <jan.schulz@devugees.org>
2018-08-16 16:06:37 +0200

    added 1st node server

--- nodejs/recursion.js
+++ nodejs/recursion.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 function f(x) {
     if(x === 0) {
         console.log('x is 0. end of recursion stack');
@@ -9,4 +11,9 @@ function f(x) {
 }
 
 f(5);
+function sum(x) {
+    if(x === 1) return 1;
+    else return sum(x-1) + x;
+}
 
+console.log( sum(5) );
