eedb194ab12e8ff4d246c1d648dba4701858f22a
Jan Schulz <jan.schulz@devugees.org>
2018-08-18 14:59:06 +0200

    Revert "added 1st node server"
    This reverts commit e25b7163c25905610b01b3ae95b1e0798d0cba37.

--- nodejs/recursion.js
+++ nodejs/recursion.js
@@ -1,5 +1,3 @@
-var fs = require('fs');
-
 function f(x) {
     if(x === 0) {
         console.log('x is 0. end of recursion stack');
@@ -11,9 +9,4 @@ function f(x) {
 }
 
 f(5);
-function sum(x) {
-    if(x === 1) return 1;
-    else return sum(x-1) + x;
-}
 
-console.log( sum(5) );
