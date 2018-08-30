3388ed5cc72b3295c67c6cea19f4fad46fa16302
Jan Schulz <jan.schulz@devugees.org>
2018-08-30 13:15:54 +0200

    solved productserver

--- nodejs/productserver/public/main.js
+++ nodejs/productserver/public/main.js
@@ -1,21 +1,33 @@
 let products = [];
 
-function loadProducts() {
+function showProducts() {
+    $('.content').empty();
+    for(let i=0; i<products.length; i++) {
+        let product = `
+            <div class="product" data-id=${products[i].id}>
+            <div class="product-title">${products[i].title}</div>
+            <div class="product-price">${products[i].price}</div>
+            </div>    
+        `;
+        $('.content').append(product);
+        if(!products[i].imageurl.startsWith('http')) {
+            $('.product[data-id='+products[i].id+']').css('background', "url('images/"+products[i].imageurl+"') no-repeat center")
+        }
+        else {
+            $('.product[data-id='+products[i].id+']').css('background', "url('"+products[i].imageurl+"') no-repeat center")
+        }
+    }
+}
+
+function loadProducts(q) {
+    let url = q ? 'http://localhost:3000/products?q='+q : 'http://localhost:3000/products';
+
     $.ajax({
-        url: 'http://localhost:3000/products',
+        url: url,
         method: 'GET',
         success: function(data) {
             products = data;            
-            for(let i=0; i<products.length; i++) {
-                let product = `
-                    <div class="product" data-id=${products[i].id}>
-                    <div class="product-title">${products[i].title}</div>
-                    <div class="product-price">${products[i].price}</div>
-                    </div>    
-                `;
-                $('.content').append(product);
-                $('.product[data-id='+products[i].id+']').css('background', "url('images/"+products[i].imageurl+".jpg') no-repeat center")
-            }
+            showProducts();
         },
         error: function(err) {
             console.log(err);
@@ -40,6 +52,7 @@ function addProduct() {
         contentType: 'application/json; charset=utf-8',
         success: function(data) {
             products.push(data);
+            showProducts();
             $('#navlink-startpage').trigger('click');
         },
         error: function(err) {
@@ -49,7 +62,6 @@ function addProduct() {
 }
 
 $('document').ready(function() {
-
 
     console.log('document ready');
     $('#navlink-startpage').on('click', function(e) {
@@ -65,6 +77,11 @@ $('document').ready(function() {
     $('#btn-addproduct').on('click', function(e) {
         addProduct();
     });   
+
+    $('#btn-search').on('click', function() {
+        let q = $('.searchbox').val();
+        loadProducts(q);
+    });
 
     loadProducts();
 });
--- nodejs/productserver/server.js
+++ nodejs/productserver/server.js
@@ -1,47 +1,50 @@
 const express = require('express');
 const fs = require('fs');
-const bodyParser = require('body-parser');
 const randomstring = require('randomstring');
+const bodyParser = require('body-parser');
 const app = express();
 
-if(!fs.existsSync(__dirname + '/products.json')) {
-    fs.writeFileSync(__dirname + '/products.json', '[]', 'utf-8');
+app.use(bodyParser.json());
+
+let productFile = __dirname + '/products.json';
+
+if(!fs.existsSync(productFile)) {
+    fs.writeFileSync(productFile, '[]', 'utf-8');
 }
 
-app.use(bodyParser.json());
-app.use('/', express.static(__dirname + '/public'));
-
-app.get('/', function(req, res) {
-    return res.send({ productapi: '1.0' });
-});
-
 app.get('/products', function(req, res) {
-    fs.readFile(__dirname + '/products.json', 'utf-8',
-        function(err, data) {
-            if(err) {
-                return res.send({ error: err });
-            }
+    fs.readFile(productFile, 'utf-8',
+    function(err, data) {
+        if(err) return res.send({err: err});
 
-            let products = JSON.parse(data);
+        let products = JSON.parse(data);
+
+        if(!req.query.q) {
             return res.send(products);
         }
-    )
+
+        let q = req.query.q.toLowerCase();
+        let resultProducts = [];
+
+        for(let i=0; i<products.length; i++) {
+            let targetString = products[i].title.toLowerCase() + products[i].description.toLowerCase();
+
+            if(targetString.includes(q)) {
+                resultProducts.push(products[i]);
+            }
+        }
+
+        return res.send(resultProducts);
+    });
 });
 
 app.post('/products', function(req, res) {
-    if(!req.body.type || !req.body.title || !req.body.description || !req.body.price) {
-        return res.send({ error: 'type, title, description and price required' });
+    if(!req.body.type || !req.body.title || !req.body.description || !req.body.price || !req.body.imageurl) {
+        return res.send({ error: 'invalid request' });
     }
 
-    if(    req.body.type.toLowerCase() !== 'book'
-        && req.body.type.toLowerCase() !== 'music'
-        && req.body.type.toLowerCase() !== 'movie') {
-            return res.send({ error: 'type is either book, music or movie' });
-        }
-    
-    fs.readFile(__dirname + '/products.json', 'utf-8', 
-    function(err, data) {
-        if(err) return res.send({ error: err });
+    fs.readFile(productFile, 'utf-8', function(err, data) {
+        if(err) return res.send({ err: err });
 
         let products = JSON.parse(data);
         let newProduct = {
@@ -49,48 +52,49 @@ app.post('/products', function(req, res) {
             description: req.body.description,
             price: req.body.price,
             type: req.body.type,
+            imageurl: req.body.imageurl,
             id: randomstring.generate(20)
         };
 
         products.push(newProduct);
         let productsJSON = JSON.stringify(products);
-        fs.writeFile(__dirname + '/products.json', productsJSON,'utf-8',
-            function(err) {
-                if(err) return res.send({ error: err });
 
-                return res.send( newProduct );
-            });
+        fs.writeFile(productFile, productsJSON, 'utf-8', function(err) {
+            if(err) return res.send({ err: err });
+
+            return res.send( newProduct );
+        })
     });
-
 });
 
 app.delete('/products/:id', function(req, res) {
-    fs.readFile(__dirname + '/products.json', 'utf-8', 
-        function(err, data) {
-            if(err) return res.send({ error: err });
+    fs.readFile(productFile, 'utf-8', function(err, data) {
+        if(err) return res.send({ err: err });
 
-            let products = JSON.parse(data);
-            let productFound = false;
-            for(let i=0; i<products.length; i++) {
-                if(products[i].id === req.params.id) {
-                    products.splice(i, 1);
-                    productFound = true;
-                    break;
-                }
+        let products = JSON.parse(data);
+        let productFound = false;
+
+        for(let i=0; i<products.length; i++) {
+            if(products[i].id === req.params.id) {
+                products.splice(i, 1);
+                productFound = true;
+                break;
             }
+        }
 
-            if(!productFound) {
-                return res.send({error: 'invalid id'});
-            }
+        if(!productFound) {
+            return res.send({ error: 'invalid id' });
+        }
 
-            let productsJSON = JSON.stringify(products);
-            fs.writeFile(__dirname + '/products.json', productsJSON,'utf-8',
-                function(err) {
-                    if(err) return res.send({ error: err });
-    
-                    return res.send({ error: 0, deleted: req.params.id });
-                });            
+        let productJSON = JSON.stringify(products);
+
+        fs.writeFile(productFile, productJSON, 'utf-8', function(err) {
+            if(err) return res.send({ err: err});
+
+            return res.send({ error: 0, deleted: req.params.id });
         });
+
+    });
 });
 
 app.listen( 3000 );
