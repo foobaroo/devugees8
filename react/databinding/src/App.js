2e722f0e8480907629a61714908d051e1de09aee
Jan Schulz <jan.schulz@devugees.org>
2018-10-18 09:55:49 +0200

    finished databindign1 example

---
+++ react/databinding/src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  
+  state = {
+    sometext: '',
+    username: '',
+    password: '',
+    loggedIn1: -1,
+    loggedIn2: -1
+  }
+  
+  onChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  login = () => {
+    if(this.state.username === 'hallo' && this.state.password === 'world') {
+      this.setState({
+        loggedIn: 1
+      })
+    }
+    else {
+      this.setState({
+        loggedIn: 0
+      })
+    }
+  }
+
+  /*
+    task: 
+
+    create a button "login" that calls a function "login" and
+    checks if the username is "hallo" and the password is "world".
+    if so, display a div underneath the login button stating with
+    green color that the login was successfull. otherwise, state with red color that the login failed.
+  
+  */
+
+  render() {
+
+    return (
+      <div className="App">
+        <h1>Databinding 1</h1>
+        <div>
+          some text: <input name="sometext" onChange={this.onChange} type="text"/>
+        </div>
+        <div>
+          you entered: {this.state.sometext}
+        </div>
+        <h1>Databinding 2</h1>    
+        <div>
+          username: <input name="username" onChange={this.onChange} type="text"/>
+        </div>
+        <div>
+          password: <input name="password" onChange={this.onChange}  type="password"/>
+        </div>        
+        <button onClick={this.login}>Login</button>
+        { this.state.loggedIn === 1 ? <div style={{color: 'green', marginLeft: '15px' }}>Login Successfull!</div> : null }
+        { this.state.loggedIn === 0 ? <div style={{color: 'red', marginLeft: '15px' }} >Login Failed!</div> : null }
+      </div>
+    );
+  }
+}
+
+export default App;
--- react/loops/src/App.js
+++ react/loops/src/App.js
@@ -7,29 +7,52 @@ class App extends Component {
     fruits: ['Apple', 'Banana', 'Lemon', 'Kiwi', 'Pineapple']
   };
 
+  removeFruit = (fruit) => {
+    // we find the index of the fruit named ${fruit}
+    const fruitIndex = this.state.fruits.findIndex((f) => {
+      return fruit === f; // returns if fruit is f
+    });
+    
+    if(fruitIndex === -1) return;
+    
+    const fruits = [...this.state.fruits];
+    fruits.splice(fruitIndex, 1);
+
+    this.setState({
+      fruits: fruits
+    });
+  }
+
+  addFruit = (fruit) => {
+    const fruits = [...this.state.fruits, fruit];
+    this.setState({
+      fruits: fruits
+    });
+  }
+
   render() {
     // task: convert the fruits array to an jsx array using
     //       the for loop and render it
 
-    let jsxArray = [];
-    for(let i=0; i<this.state.fruits.length; i++) {
-      jsxArray.push( <li>{this.state.fruits[i]}</li> );
-    }
+    // let jsxArray = [];
+    // for(let i=0; i<this.state.fruits.length; i++) {
+    //   jsxArray.push( <li>{this.state.fruits[i]}</li> );
+    // }
 
-    const arr = [<li>1</li>, <li>2</li>];
+    // const arr = [<li>1</li>, <li>2</li>];
     return (
       <div className="App">
       <h1>Loops Part 1</h1>
+      <button onClick={this.removeFruit.bind(this, 'Apple')}>Remove Apple</button>
+      {/* task 
+        Add a new button "AddMango" that adds a Mango the fruits array!
+      */}
+      <button onClick={this.addFruit.bind(this, 'Mango')}>Add Mango</button>
       <ul>
         {this.state.fruits.map( (fruit) => {
           return <li>{fruit}</li>
         })}
-        {arr}
       </ul>
-
-      <ul>
-        {jsxArray}
-      </ul>      
       </div>
     );
   }
