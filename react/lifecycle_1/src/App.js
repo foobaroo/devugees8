b67480cc8c4be5e1d4bf099d90beb620f7470952
Jan Schulz <jan.schulz@devugees.org>
2018-10-25 14:12:32 +0200

    added lifecycle example

--- react/lifecycle_1/src/App.js
+++ react/lifecycle_1/src/App.js
@@ -1,35 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import Test from './components/Test';
 
 class App extends Component {
+  
+  state = {
+    showTest: true,
+    x: 0
+  };
+  
+  toggleTest = () => {
+    this.setState({
+      showTest: !this.state.showTest
+    })
+  }
+
+  incrementX = () => {
+    this.setState({
+      x: this.state.x + 1
+    })
+  }
+
   render() {
         
-    const name = 'Larry';
-    const a = 5;
-    const b = 7;
-    const showHalloWorld = false;
-    const num1 = 42;
-    const num2 = 50;
-    
-    let math = <h4>{num1}+{num2} = {num1 + num2}</h4>;
-    let hwStr = 'hallo world';
-    let hw = <h1>{hwStr}</h1>;
-
     return (
       <div className="App">
-        <h1>The App Component</h1>
-        <h4>Hallo {name}</h4>
-        {/* comment */}
-        <h4>Hallo {name.toUpperCase()}</h4>
-        <h4>1 + 1 = {1+1}</h4>
-        <h4>a + b = {a+b}</h4>
-        <h4>Nothing: {null}</h4>
-        { showHalloWorld ? <h4>Hallo World</h4> : null }
-        {math}
-        {/* task: create a variable that contains 'hallo world'. then display halloworld inside a h1 tag that you save 
-        in a variable */}
-        {hw}
+        <h1>The Component Lifecycle</h1>
+        <button onClick={this.toggleTest}>{ this.state.showTest ? 'unmount ' : 'mount '}Test</button>
+        <button onClick={this.incrementX}>incrementX</button>
 
+        { this.state.showTest ? <Test x={this.state.x} /> : null }
       </div>
     );
   }
